65638c109b1234e72f0c9714c19c40666c36dbd1
Laddeus <33806862+Laddeus@users.noreply.github.com>
2021-05-22 00:26:08 +0300

    figured out how praw works and changed stuff

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from os.path import dirname, join
 import praw, time, calendar
 from praw import Reddit
 from prettytable import PrettyTable
-from praw.models import MoreComments
+from praw.models import MoreComments, Comment, Submission
 from colorama import init, Fore, Back, Style
 from dotenv import load_dotenv
+from typing import List
 
 load_dotenv(join(dirname(__file__), '.env'))
 reddit_app_client_id = os.environ.get("REDDIT_APP_CLIENT_ID")
 reddit_app_secret = os.environ.get("REDDIT_APP_SECRET")
-
-print(reddit_app_client_id)
-print(reddit_app_secret)
 
 init()
 reddit = praw.Reddit(
@@ -23,49 +21,43 @@ reddit = praw.Reddit(
 
 print(reddit.read_only)
 
-localtime = calendar.timegm(time.gmtime())
 commentTable = PrettyTable()
 commentTable.field_names = ["Subreddit", "ScoreMin", "Body", "Link"]
 
 
 def find_rising_posts(reddit: Reddit):
-    comments_tree = []
+    comments_to_search: List[Comment] = []
 
-    for submission in reddit.subreddit("all").hot(limit=20):
-        submission.comment_sort = "top"
-        submission.comment_limit = "10"
-        top_level_comments = submission.comments.list()
+    submission: Submission
+    for submission in reddit.subreddit("all").hot(limit=10): # iterate over submissions in r/all (api call)
+        submission.comment_sort = "best"
+        submission.comment_limit = "40"
+        submission_comments = submission.comments.list() # request for current submission's comments list (direct + nested) (api call)
 
-        print("Got comment")
+        print("Got submission comments")
+        number_of_comments = 0
 
-        for comment in top_level_comments:
-            if not isinstance(comment, MoreComments):
-                comments_tree.append(comment)
+        for comment in submission.comments:
+            if isinstance(comment, Comment):
+                number_of_comments += 1
 
-    while comments_tree:
-        current_comment = comments_tree.pop(0)
+        print(number_of_comments)
 
-        if isinstance(current_comment, MoreComments):
-            continue
+        comment: Comment
+        for comment in submission_comments: # for each comment of current submission append into comments_to_search
+            if isinstance(comment, Comment):
+                comments_to_search.append(comment)
 
-        # if current_comment.body == "[removed]":
-        #     continue
-
-        comment_time_since_creation_minutes = round((localtime - current_comment.created_utc) / 60, 2) + 1
-        if comment_time_since_creation_minutes < 120 and current_comment.score > comment_time_since_creation_minutes:
-            formatted_body_message = Back.YELLOW + current_comment.body + Style.RESET_ALL
-            score_min = int(round(current_comment.score / comment_time_since_creation_minutes, 0))
-            full_permalink = 'http://www.reddit.com' + current_comment.permalink
-            commentTable.add_row([submission.subreddit, score_min, formatted_body_message[:36], full_permalink])
+    localtime = calendar.timegm(time.gmtime())
+    for comment in comments_to_search: # iterate over all comments gathered and do idiot checks
+        comment_time_since_creation_minutes = round((localtime - comment.created_utc) / 60, 2) + 1
+        if comment_time_since_creation_minutes < 60 and comment.score > comment_time_since_creation_minutes:
+            formatted_body_message = Back.YELLOW + comment.body + Style.RESET_ALL
+            score_min = round(comment.score / comment_time_since_creation_minutes, 2)
+            full_permalink = 'http://www.reddit.com' + comment.permalink
+            commentTable.add_row([comment.subreddit, score_min, formatted_body_message[:36], full_permalink])
             print(Style.RESET_ALL)
 
-        current_comment.reply_sort = "new"
-        # current_comment.refresh()
-        comment_replies = current_comment.replies.list()
-
-        for reply in comment_replies:
-            if not isinstance(reply, MoreComments):
-                comments_tree.append(reply)
 
 
 print(reddit.auth.limits)
